b303130bfd131a751ae55dbf28f038a8f390af83
tanfuc <nguyentannphuc@gmail.com>
2025-05-15 20:03:53 +0700

    change routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import UserRouter from '../routes/user.js';
 import HomeRouter from '../routes/home.js';
 import CourseRouter from '../routes/course.js';
-import Dashboard from '../routes/dashboard.js';
+import Teacher from '../routes/teacher.js';
+import Admin from '../routes/admin.js';
 
 const route = (app) => {
   app.use('/courses', CourseRouter);
   app.use('/auth', UserRouter);
-  app.use('/dashboard', Dashboard);
+  app.use('/teacher', Teacher);
+  app.use('/admin', Admin);
   app.use('/', HomeRouter);
 };
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
+import { registerValidation } from '../middleware/authValidator.js';
 import UserController from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.post('/forgot-password', UserController.forgotPassword);
-router.post('/register', UserController.confirmregister);
+router.post('/register', registerValidation, UserController.registerValidation);
 router.post('/login', UserController.confirmlogin);
 router.post('/reset-password/:token', UserController.postResetPassword);
 router.get('/forgot-password', (req, res) => res.render('forgot-password'));
